ba64999c262c91f930ce7fb514c0978ffe6e018c
FaithKwamboka <okongofaith3@gmail.com>
2022-06-18 11:05:29 +0300

    Feat: Add urls

--- hood/urls.py
+++ hood/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('all-hoods/', views.hoods, name='hood'),
     path('new-hood/', views.create_hood, name='new-hood'),
     path('<hood_id>/members', views.hood_members, name='members'),
+    path('profile/<username>', views.profile, name='profile'),
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
 ]
