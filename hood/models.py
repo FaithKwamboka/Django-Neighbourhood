fa917a8a288260ece27894d9e67d9f4ba5f0c641
FaithKwamboka <okongofaith3@gmail.com>
2022-06-19 16:08:25 +0300

    Update models

--- hood/models.py
+++ hood/models.py
@@ -9,11 +9,11 @@ class NeighbourHood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
     admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
-    hood_logo=ImageField(manual_crop='1280x720')
-    # hood_logo = models.ImageField(upload_to='images/')
+    hood_logo = models.ImageField(upload_to='images/')
     description = models.TextField()
+    health_tell = models.IntegerField(null=True, blank=True)
     police_number = models.IntegerField(null=True, blank=True)
-    health_number = models.IntegerField(null=True, blank=True)
+
     
 
     def __str__(self):
@@ -34,8 +34,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=80, blank=True)
     bio = models.TextField(max_length=254, blank=True)
-    profile_picture=ImageField(manual_crop='1280x720')
-    # profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
     location = models.CharField(max_length=50, blank=True, null=True)
     neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
