3eab9f7d958fc1e79b1f09e619b58bbc3d2fa845
FaithKwamboka <okongofaith3@gmail.com>
2022-06-19 23:58:12 +0300

    Add tests

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import *
+import unittest
 
-# Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='kwash', password='kwamboka')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
+
+
+if __name__ == '__main__':
+    unittest.main()  
