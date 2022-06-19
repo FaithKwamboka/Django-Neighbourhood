a83aeee25767832d2ee5efedf3a7911350505d57
FaithKwamboka <okongofaith3@gmail.com>
2022-06-19 18:46:05 +0300

    Feat: Add search url

--- hood/urls.py
+++ hood/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
     path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
     path('<hood_id>/new-post', views.create_post, name='post'),
+    path('search/', views.search_business, name='search'),
 ]
